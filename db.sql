0c961ec7c919924cac74fc215c735f9ffd503c34
liudding <liuding.ld@foxmail.com>
2020-11-16 12:55:22 +0800

    update db visits table structure

--- db.sql
+++ db.sql
@@ -5,8 +5,8 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- Table structure for shorturls
 -- ----------------------------
 CREATE TABLE IF NOT EXISTS `short_urls` (
-  `code` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '短链接码',
-  `url` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '原始链接',
+  `code` varchar(20) NOT NULL COMMENT '短链接码',
+  `url` varchar(255) NOT NULL COMMENT '原始链接',
   `visits` int unsigned NOT NULL DEFAULT '0' COMMENT '被访问的次数',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   PRIMARY KEY (`code`)
@@ -18,9 +18,12 @@ CREATE TABLE IF NOT EXISTS `short_urls` (
 CREATE TABLE IF NOT EXISTS `visits` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
   `short_url` varchar(20) NOT NULL,
-  `ip_address` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '访问者的 ip',
-  `referer` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT 'http header referer',
-  `user_agent` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT 'http header',
+  `ip_address` varchar(40) NOT NULL COMMENT '访问者的 ip',
+  `browser` varchar(50) NOT NULL default ''COMMENT '',
+  `device` varchar(50) NOT NULL default '',
+  `os` varchar(50) NOT NULL default '',
+  `referer` varchar(255) NOT NULL COMMENT 'http header referer',
+  `user_agent` varchar(255) NOT NULL COMMENT 'http header',
   `visited_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '访问时间',
   PRIMARY KEY (`id`),
   KEY `key_shorturl` (`short_url`)
