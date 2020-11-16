fc4b5884e374d25059729e554a85d94f181fee2e
liudding <liuding.ld@foxmail.com>
2020-11-16 17:15:52 +0800

    add view_admin.php file

---
+++ views/view_admin.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <div id="main">
+        
+    </div>
+</body>
+
+
+<style>
+    html {
+        height: 100%;
+    }
+
+    body {
+        font-family: 'Lato', sans-serif;
+        color: #888;
+        margin: 0;
+    }
+
+    #main {
+        display: table;
+        width: 100%;
+        height: 100vh;
+        text-align: center;
+    }
+</style>
+
+</html>
